db2de0810d5bca8f58ad355d097dfff1bb3cb03c
Daniel <daniel@dantleech.com>
2023-10-10 10:11:04 +0100

    More passing test

--- src/Widget/Block.php
+++ src/Widget/Block.php
@@ -21,7 +21,19 @@ final class Block
 
     public function inner(Area $area): Area
     {
-        return $area;
+        $x = $area->position->x;
+        $y = $area->position->y;
+        $width = $area->width;
+        $height = $area->height;
+        if ($this->borders & Borders::LEFT) {
+            $x = min($x + 1, $area->right());
+            $width = max(0, $width - 1);
+        }
+        if ($this->borders & Borders::TOP) {
+            $y = min($y + 1, $area->bottom());
+            $height = max(0, $height-1);
+        }
+        return Area::fromPrimitives($x, $y, $width, $height);
     }
 
     /**
