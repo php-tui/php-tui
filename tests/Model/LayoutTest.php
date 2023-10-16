bc8f2e450979cabebfb40abc760f7ace448c2fad
Daniel Leech <daniel@dantleech.com>
2023-10-16 14:33:49 +0100

    Failing counterpart for RS layout  test

--- tests/Model/LayoutTest.php
+++ tests/Model/LayoutTest.php
@@ -32,4 +32,21 @@ class LayoutTest extends TestCase
             )
         );
     }
+    
+    public function testSplitEquallyInUnderspecifiedCase(): void
+    {
+        $target = Area::fromPrimitives(100, 100, 10, 10);
+        $layout = Layout::default()
+            ->direction(Direction::Horizontal)
+            ->constraints([
+                Constraint::min(2),
+                Constraint::min(2),
+                Constraint::min(0),
+            ])
+            ->split($target);
+
+        self::assertEquals([100,100,2,10], $layout->get(0)->toArray());
+        self::assertEquals([102,100,2,10], $layout->get(1)->toArray());
+        self::assertEquals([104,100,6,10], $layout->get(2)->toArray());
+    }
 }
