4de8d669fd4a6928b62c03552ab0651b7c371b0a
Daniel Leech <daniel@dantleech.com>
2023-10-16 15:32:55 +0100

    Need to impleent Paragraph

--- example/blocks.php
+++ example/blocks.php
@@ -6,6 +6,7 @@ use DTL\PhpTui\Model\Backend\DummyBackend;
 use DTL\PhpTui\Model\Buffer;
 use DTL\PhpTui\Model\Constraint;
 use DTL\PhpTui\Model\Direction;
+use DTL\PhpTui\Model\Exception\TodoException;
 use DTL\PhpTui\Model\Layout;
 use DTL\PhpTui\Model\Terminal;
 
@@ -14,7 +15,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $backend = new DummyBackend(128, 33);
 $terminal = Terminal::fullscreen($backend);
 $terminal->draw(function (Buffer $buffer): void {
-    $area = $buffer->area();
+    [$titleArea, $layout] = calculate_layout($buffer->area());
+    render_title($buffer, $titleArea);
+
+});
+
+function calculate_layout(Area $area): array
+{
     $layout = Layout::default()
         ->direction(Direction::Vertical)
         ->constraints([
@@ -23,11 +30,25 @@ $terminal->draw(function (Buffer $buffer): void {
         ])
         ->split($area);
     $titleArea = $layout->get(0);
-    $mainAreas = Layout::default()
+    $mainAreas = array_map(function (Area $area) {
+        return Layout::default()
+            ->direction(Direction::Horizontal)
+            ->constraints([
+                Constraint::percentage(50),
+                Constraint::percentage(50),
+            ])
+            ->split($area)
+            ->toArray();
+    }, Layout::default()
         ->direction(Direction::Vertical)
         ->constraints(array_map(fn () => Constraint::max(9), array_fill(0, 9, true)))
-        ->split($layout->get(1));
-    dd($mainAreas);
+        ->split($layout->get(1))
+        ->toArray());
+
+    return [$titleArea, $mainAreas];
+}
 
 
-});
+function render_title(Buffer $buffer, Area $area): void {
+    throw new TodoException('Paragraph!');
+}
