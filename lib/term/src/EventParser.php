e0042a9df6a00f26ef932411bde26eb3ce35af4b
Daniel Leech <daniel@dantleech.com>
2023-10-23 21:44:26 +0100

    Failing test cases for keys

--- lib/term/src/EventParser.php
+++ lib/term/src/EventParser.php
@@ -2,11 +2,19 @@
 
 namespace PhpTui\Term;
 
-use Event;
+use PhpTui\Term\Event\KeyCode;
+use PhpTui\Term\Event\KeyEvent;
 
 class EventParser
 {
+    /**
+     * @var string[]
+     */
     private array $buffer = [];
+
+    /**
+     * @var Event[]
+     */
     private array $events = [];
 
     /**
@@ -19,24 +27,56 @@ class EventParser
         return $events;
     }
 
-    public function advance(string $line): void
+    public function advance(string $line, bool $more): void
     {
         // split string into bytes
         $bytes = str_split($line);
 
-        foreach ($bytes as $byte) {
+        foreach ($bytes as $index => $byte) {
+            $more = $index + 1 < strlen($line) || $more;
+
+            $this->buffer[] = $byte;
             try {
-                $event = $this->parseEvent();
+                $event = $this->parseEvent($this->buffer, $more);
                 if ($event === null) {
                     continue;
                 }
+                $this->events[] = $event;
             } catch (ParseError $error) {
                 continue;
             }
         }
     }
 
-    private function parseEvent(): ?Event
+    /**
+     * @param string[] $buffer
+     */
+    private function parseEvent(array $buffer, bool $inputAvailable): ?Event
     {
+        if ($buffer === []) {
+            return null;
+        }
+
+        return match ($buffer[0]) {
+            "\x1B" => $this->parseEsc($buffer, $inputAvailable),
+            default => throw new ParseError('TODO'),
+        };
+    }
+
+    /**
+     * @param string[] $buffer
+     */
+    private function parseEsc(array $buffer, bool $inputAvailable): ?Event
+    {
+        if (count($buffer) === 1) {
+            if ($inputAvailable) {
+                // _could_ be an escape sequence
+                return null;
+            }
+
+            return KeyEvent::new(KeyCode::Esc);
+        }
+
+        return null;
     }
 }
--- lib/term/tests/EventParserTest.php
+++ lib/term/tests/EventParserTest.php
@@ -14,23 +14,120 @@ class EventParserTest extends TestCase
     /**
      * @dataProvider provideParse
      */
-    public function testParse(string $line, Event $event): void
+    public function testParse(string $line, ?Event $expected, ?bool $moreInput = false): void
     {
         $parser = new EventParser();
-        $parser->advance($line);
+        $parser->advance($line, $moreInput);
         $events = $parser->drain();
+        if (null === $expected) {
+            self::assertCount(0, $events);
+            return;
+        }
         self::assertCount(1, $events);
-        self::assertEquals($events, $events[0]);
+        self::assertEquals($expected, $events[0]);
     }
 
     /**
-     * @return Generator<array{string,KeyEvent}>
+     * @return Generator<array{0:string,1:?KeyEvent,2?:bool}>
      */
     public static function provideParse(): Generator
     {
-        yield [
-            "\x1Bc",
-            KeyEvent::new(KeyCode::Esc)
+        yield 'esc' => [
+            "\x1B",
+            KeyEvent::new(KeyCode::Esc),
+        ];
+        yield 'possible esc sequence' => [
+            "\x1B",
+            null,
+            true,
+        ];
+        yield 'Backspace' => [
+            '',
+            KeyEvent::new(KeyCode::Backspace),
+        ];
+        /// Enter key.
+        yield 'Enter' => [
+            '',
+            KeyEvent::new(KeyCode::Enter),
+        ];
+        /// Left arrow key.
+        yield 'Left' => [
+            '',
+            KeyEvent::new(KeyCode::Left),
+        ];
+        /// Right arrow key.
+        yield 'Right' => [
+            '',
+            KeyEvent::new(KeyCode::Right),
+        ];
+        /// Up arrow key.
+        yield 'Up' => [
+            '',
+            KeyEvent::new(KeyCode::Up),
+        ];
+        /// Down arrow key.
+        yield 'Down' => [
+            '',
+            KeyEvent::new(KeyCode::Down),
+        ];
+        /// Home key.
+        yield 'Home' => [
+            '',
+            KeyEvent::new(KeyCode::Home),
+        ];
+        /// End key.
+        yield 'End' => [
+            '',
+            KeyEvent::new(KeyCode::End),
+        ];
+        /// Page up key.
+        yield 'PageUp' => [
+            '',
+            KeyEvent::new(KeyCode::PageUp),
+        ];
+        /// Page down key.
+        yield 'PageDown' => [
+            '',
+            KeyEvent::new(KeyCode::PageDown),
+        ];
+        /// Tab key.
+        yield 'Tab' => [
+            '',
+            KeyEvent::new(KeyCode::Tab),
+        ];
+        /// Shift + Tab key.
+        yield 'BackTab' => [
+            '',
+            KeyEvent::new(KeyCode::BackTab),
+        ];
+        /// Delete key.
+        yield 'Delete' => [
+            '',
+            KeyEvent::new(KeyCode::Delete),
+        ];
+        /// Insert key.
+        yield 'Insert' => [
+            '',
+            KeyEvent::new(KeyCode::Insert),
+        ];
+        /// F key.
+        ///
+        /// `KeyCode::F(1)` represents F1 key; etc.
+        yield 'FKey' => [
+            '',
+            KeyEvent::new(KeyCode::FKey),
+        ];
+        /// A character.
+        ///
+        /// `KeyCode::Char('c')` represents `c` character; etc.
+        yield 'Char' => [
+            '',
+            KeyEvent::new(KeyCode::Char),
+        ];
+        /// Null.
+        yield 'Null' => [
+            '',
+            KeyEvent::new(KeyCode::Null),
         ];
     }
 }
