5870075352e5604396e7cf678e8cbebcc9e53047
Daniel Leech <daniel@dantleech.com>
2023-10-31 09:50:30 +0000

    Fix max issue

--- src/Widget/Canvas/Shape/Sprite.php
+++ src/Widget/Canvas/Shape/Sprite.php
@@ -29,25 +29,33 @@ class Sprite implements Shape
 
     public function draw(Painter $painter): void
     {
-        $maxX = max(...array_map(fn (string $row) => mb_strlen($row), $this->rows));
-        foreach (array_reverse($this->rows) as $y => $row) {
-            $chars = mb_str_split($row);
-            for ($x = 1; $x < $maxX; $x+=1/$this->density) {
-                $intX = intval(floor($x));
-                if (!isset($chars[$intX])) {
-                    $chars[$intX] = $this->alphaChar;
+        $maxX = max(0, ...array_map(fn (string $row) => mb_strlen($row), $this->rows));
+        $densityRatio = 1/$this->density;
+        foreach (array_reverse($this->rows) as $cellY => $row) {
+            // fill the cell (step 1) from top to bottom.
+            // if density = 4 and cell is 0,0 start at 0,75 then 0,5, 0.25, 0
+            for ($y = $cellY + 1 - $densityRatio; $y >= $cellY; $y -= $densityRatio) {
+                $chars = mb_str_split($row);
+
+                // fill the cell from left to right
+                // if desity = 4 and 0,0 then 0,0, 0.25, 0.5, 0.75
+                for ($x = 0; $x < $maxX; $x += $densityRatio) {
+                    $cellX = intval(floor($x));
+                    if (!isset($chars[$cellX])) {
+                        $chars[$cellX] = $this->alphaChar;
+                    }
+                    if ($chars[$cellX] === $this->alphaChar) {
+                        continue;
+                    }
+                    $point = $painter->getPoint(FloatPosition::at(
+                        $this->position->x + $x * $this->xScale,
+                        $this->position->y + $y * $this->yScale,
+                    ));
+                    if (null === $point) {
+                        continue;
+                    }
+                    $painter->paint($point, $this->color);
                 }
-                if ($chars[$intX] === $this->alphaChar) {
-                    continue;
-                }
-                $point = $painter->getPoint(FloatPosition::at(
-                    $this->position->x + $x * $this->xScale,
-                    $this->position->y + $y * $this->yScale,
-                ));
-                if (null === $point) {
-                    continue;
-                }
-                $painter->paint($point, $this->color);
             }
 
         }
