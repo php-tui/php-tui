b56c9ba91d8e1a7c500f3ef49dffb327d8c0bda4
Daniel Leech <daniel@dantleech.com>
2023-10-31 10:56:22 +0000

    ANSI color from index

---
+++ src/Widget/RawWidget.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PhpTui\Tui\Widget;
+
+use Closure;
+use PhpTui\Tui\Model\Area;
+use PhpTui\Tui\Model\Buffer;
+use PhpTui\Tui\Model\Widget;
+
+
+/**
+ * This widget allows you to write directly to the buffer through a closure.
+ */
+final class RawWidget implements Widget
+{
+    public function __construct(
+        /**
+         * The callback for writing to the buffer.
+         * @var Closure(Buffer $buffer, Area $area):void
+         */
+        private Closure $widget
+    ) {}
+
+    /**
+     * @param Closure(Buffer $buffer, Area $area):void $closure
+     */
+    public static function new(Closure $closure): self
+    {
+        return new self($closure);
+    }
+
+    public function render(Area $area, Buffer $buffer): void
+    {
+        ($this->widget)($buffer, $area);
+    }
+}
