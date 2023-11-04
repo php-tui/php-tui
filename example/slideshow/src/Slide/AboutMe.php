5f1b9d29a042271ca937e6b92977e9c145a9abc7
Daniel Leech <daniel@dantleech.com>
2023-11-04 18:51:57 +0000

    Use list

--- example/slideshow/src/Slide/AboutMe.php
+++ example/slideshow/src/Slide/AboutMe.php
@@ -2,17 +2,15 @@
 
 namespace PhpTui\Tui\Example\Slideshow\Slide;
 
-use PhpTui\Cassowary\Variable;
 use PhpTui\Tui\Adapter\Bdf\FontRegistry;
 use PhpTui\Tui\Adapter\Bdf\Shape\TextShape;
 use PhpTui\Tui\Adapter\ImageMagick\Shape\ImageShape;
 use PhpTui\Tui\Example\Slideshow\Slide;
 use PhpTui\Tui\Model\AnsiColor;
-use PhpTui\Tui\Model\Buffer;
 use PhpTui\Tui\Model\Constraint;
 use PhpTui\Tui\Model\Direction;
 use PhpTui\Tui\Model\Marker;
-use PhpTui\Tui\Model\Position;
+use PhpTui\Tui\Model\Style;
 use PhpTui\Tui\Model\Widget;
 use PhpTui\Tui\Model\Widget\FloatPosition;
 use PhpTui\Tui\Model\Widget\Text;
@@ -20,8 +18,8 @@ use PhpTui\Tui\Widget\Block;
 use PhpTui\Tui\Widget\Block\Padding;
 use PhpTui\Tui\Widget\Canvas;
 use PhpTui\Tui\Widget\Grid;
-use PhpTui\Tui\Widget\Paragraph;
-use PhpTui\Tui\Widget\RawWidget;
+use PhpTui\Tui\Widget\ItemList;
+use PhpTui\Tui\Widget\ItemList\ListItem;
 
 final class AboutMe implements Slide
 {
@@ -69,14 +67,20 @@ final class AboutMe implements Slide
 
     private function text(): Widget
     {
-        return Paragraph::fromString(implode("\n", [
-            '- PHP Developer',
-            '- PHPBench',
-            '- Phpactor',
-            '- PHP-TUI',
-            '- Lived in Berlin',
-            '- Moved back to Weymouth',
-        ]));
+        return Block::default()->padding(Padding::fromPrimitives(5, 5, 5, 5))->widget(
+            ItemList::default()
+            ->select(0)
+            ->highlightSymbol('')
+            ->highlightStyle(Style::default()->bg(AnsiColor::LightCyan)->fg(AnsiColor::Black))
+            ->items(
+                ListItem::new(Text::fromString('- PHP Developer')),
+                ListItem::new(Text::fromString('- PHPBench')),
+                ListItem::new(Text::fromString('- Phpactor')),
+                ListItem::new(Text::fromString('- PHP-TUI')),
+                ListItem::new(Text::fromString('- Lived in Berlin')),
+                ListItem::new(Text::fromString('- Moved to Weymouth')),
+            )
+        );
     }
 
     private function me(): Widget
