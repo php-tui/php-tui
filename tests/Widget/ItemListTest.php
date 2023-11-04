1c0a5ffe22c8d8f921a94f8512561b896e4dbc38
Daniel Leech <daniel@dantleech.com>
2023-11-04 22:03:47 +0000

    Revert "Revert "Failing test for itemlist bounds""
    This reverts commit 824901fa94d66bb9259c04c046302a073ae64465.

--- tests/Widget/ItemListTest.php
+++ tests/Widget/ItemListTest.php
@@ -127,5 +127,20 @@ class ItemListTest extends TestCase
                 '>>4',
             ]
         ];
+        yield 'with out of range offset' => [
+            Area::fromDimensions(3, 2),
+            ItemList::default()
+                ->offset(-1)
+                ->items(
+                    ListItem::new(Text::fromString('1')),
+                    ListItem::new(Text::fromString('2')),
+                    ListItem::new(Text::fromString('3')),
+                    ListItem::new(Text::fromString('4')),
+                ),
+            [
+                '  3',
+                '>>4',
+            ]
+        ];
     }
 }
