4580e7385a54d533fcdbe771e4f40999c7734e42
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-11-05 15:25:25 -0300

    Perf: improve `BdfTokenStream` logic (#66)
    * Perf: improve `BdfTokenStream` logic
    
    * Use `\n` instead of PHP_EOL
    
    * Add Bench test
    
    * Chore tests

---
+++ lib/bdf/tests/Benchmark/BdfParserBench.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace PhpTui\Bdf\Tests\Benchmark;
+
+use RuntimeException;
+use PhpTui\BDF\BdfParser;
+use PhpBench\Attributes\Iterations;
+use PhpBench\Attributes\Revs;
+
+#[Iterations(10)]
+#[Revs(25)]
+final class BdfParserBench
+{
+    private string $contents;
+
+    public function __construct()
+    {
+        $contents = file_get_contents(__DIR__ . '/../../fonts/6x10.bdf');
+
+        if (false === $contents) {
+            throw new RuntimeException('Could not read file');
+        }
+
+        $this->contents = $contents;
+    }
+
+    public function benchParseRealFont(): void
+    {
+        (new BdfParser())->parse($this->contents);
+    }
+}
