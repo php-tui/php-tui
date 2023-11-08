aff01c176c15df734801713934b27474e0872ddb
Daniel Leech <daniel@dantleech.com>
2023-11-08 10:24:03 +0000

    Add closure shape example

---
+++ example/docs/shape/closureShape.php
@@ -0,0 +1,28 @@
+<?php
+
+use PhpTui\Tui\Adapter\PhpTerm\PhpTermBackend;
+use PhpTui\Tui\Model\Display;
+use PhpTui\Tui\Model\Marker;
+use PhpTui\Tui\Model\Widget\Line;
+use PhpTui\Tui\Widget\Canvas;
+use PhpTui\Tui\Widget\Canvas\Painter;
+use PhpTui\Tui\Widget\Canvas\Shape\ClosureShape;
+
+require 'vendor/autoload.php';
+
+$display = Display::fullscreen(PhpTermBackend::new());
+$display->drawWidget(
+    Canvas::fromIntBounds(-1, 21, -1, 21)
+        ->marker(Marker::Dot)
+        ->draw(
+            new ClosureShape(
+                function (Painter $painter): void {
+                    $painter->context->print(
+                        0,
+                        0,
+                        Line::fromString('Hello World')
+                    );
+                }
+            )
+        )
+);
