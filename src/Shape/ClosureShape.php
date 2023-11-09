ec4cc8a35bddcf06e1c732294ebd29e0bf8e2350
Daniel Leech <daniel@dantleech.com>
2023-11-09 09:55:12 +0000

    Failing image set

--- src/Shape/ClosureShape.php
+++ src/Shape/ClosureShape.php
@@ -11,12 +11,7 @@ class ClosureShape implements Shape
     /**
      * @param Closure(Painter):void $closure
      */
-    public function __construct(private Closure $closure)
+    public function __construct(public readonly Closure $closure)
     {
-    }
-
-    public function draw(Painter $painter): void
-    {
-        ($this->closure)($painter);
     }
 }
--- src/Shape/DefaultShapeSet.php
+++ src/Shape/DefaultShapeSet.php
@@ -15,6 +15,7 @@ class DefaultShapeSet implements ShapeSet
             new PointsPainter(),
             new RectanglePainter(),
             new SpritePainter(),
+            new ClosurePainter(),
         ];
     }
 }
