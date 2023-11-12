a1b5895ac02d358612a740da9a84a731d9d6e538
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-11-12 15:19:14 -0300

    Chore: Make `Black` start at `0` in `AnsiColor` (#92)
    * Chore: Make `Black` start at `0` in `AnsiColor`
    
    * Assert exception type

--- tests/Model/AnsiColorTest.php
+++ tests/Model/AnsiColorTest.php
@@ -4,18 +4,19 @@ namespace PhpTui\Tui\Tests\Model;
 
 use PHPUnit\Framework\TestCase;
 use PhpTui\Tui\Model\AnsiColor;
+use ValueError;
 
 class AnsiColorTest extends TestCase
 {
     public function testFromIndex(): void
     {
-        self::assertEquals(AnsiColor::Black, AnsiColor::fromIndex(0));
-        self::assertEquals(AnsiColor::White, AnsiColor::fromIndex(15));
+        self::assertEquals(AnsiColor::Black, AnsiColor::from(0));
+        self::assertEquals(AnsiColor::White, AnsiColor::from(15));
     }
 
     public function testFromIndexOutOfBounds(): void
     {
-        $this->expectExceptionMessage('ANSI color with index "16" does not exist, must be in range of 0-15');
-        self::assertEquals(AnsiColor::White, AnsiColor::fromIndex(16));
+        $this->expectException(ValueError::class);
+        self::assertEquals(AnsiColor::White, AnsiColor::from(16));
     }
 }
