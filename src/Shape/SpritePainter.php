ae2714ec30cc567d2b6165bb450b4e4081fe6f4a
Daniel Leech <daniel@dantleech.com>
2023-11-14 10:43:09 +0000

    FAiling test for scale

--- src/Shape/SpritePainter.php
+++ src/Shape/SpritePainter.php
@@ -16,34 +16,32 @@ class SpritePainter implements ShapePainter
         }
 
         $maxX = max(0, ...array_map(fn (string $row) => mb_strlen($row), $shape->rows));
+        $pixelWidth = $shape->xScale;
+        $pixelHeight = $shape->yScale;
+        
         $densityRatio = 1;
-        foreach (array_reverse($shape->rows) as $cellY => $row) {
-            // fill the cell (step 1) from top to bottom.
-            // if density = 4 and cell is 0,0 start at 0,75 then 0,5, 0.25, 0
-            for ($y = $cellY + 1 - $densityRatio; $y >= $cellY; $y -= $densityRatio) {
-                $chars = mb_str_split($row);
+        foreach (array_reverse($shape->rows) as $y => $row) {
+            $chars = mb_str_split($row);
 
-                // fill the cell from left to right
-                // if desity = 4 and 0,0 then 0,0, 0.25, 0.5, 0.75
-                for ($x = 0; $x < $maxX; $x += $densityRatio) {
-                    $cellX = intval(floor($x));
-                    if (!isset($chars[$cellX])) {
-                        $chars[$cellX] = $shape->alphaChar;
-                    }
-                    if ($chars[$cellX] === $shape->alphaChar) {
-                        continue;
-                    }
-                    $point = $painter->getPoint(FloatPosition::at(
-                        1 + $shape->position->x + $x * $shape->xScale,
-                        $shape->position->y + $y * $shape->yScale,
-                    ));
-                    if (null === $point) {
-                        continue;
-                    }
-                    $painter->paint($point, $shape->color);
+            // fill the cell from left to right
+            // if desity = 4 and 0,0 then 0,0, 0.25, 0.5, 0.75
+            foreach ($chars as $x => $char) {
+                $cellX = intval(floor($x));
+                if (!isset($chars[$cellX])) {
+                    $chars[$cellX] = $shape->alphaChar;
                 }
+                if ($chars[$cellX] === $shape->alphaChar) {
+                    continue;
+                }
+                $point = $painter->getPoint(FloatPosition::at(
+                    1 + $shape->position->x + $x * $shape->xScale,
+                    $shape->position->y + $y * $shape->yScale,
+                ));
+                if (null === $point) {
+                    continue;
+                }
+                $painter->paint($point, $shape->color);
             }
-
         }
     }
 }
