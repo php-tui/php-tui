ab26453edcbab71c46ad66ac32fe264432c507b3
Daniel Leech <daniel@dantleech.com>
2023-11-15 23:02:45 +0000

    Fix example

--- example/docs/widget/gaugeWidget.php
+++ example/docs/widget/gaugeWidget.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 use PhpTui\Tui\DisplayBuilder;
 use PhpTui\Tui\Extension\Core\Widget\GaugeWidget;
+use PhpTui\Tui\Model\AnsiColor;
+use PhpTui\Tui\Model\Style;
 
 require 'vendor/autoload.php';
 
 $display = DisplayBuilder::default()->build();
 $display->draw(
-    GaugeWidget::default()->ratio(0.5)
+    GaugeWidget::default()->ratio(0.25)->style(Style::default()->fg(AnsiColor::Yellow))
 );
