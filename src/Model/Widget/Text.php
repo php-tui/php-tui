899a61b8a8bb631aa4239725c4aabd4f75ccf72f
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-11-19 13:12:56 -0300

    Fix text parsing

--- src/Model/Widget/Text.php
+++ src/Model/Widget/Text.php
@@ -29,9 +29,18 @@ final class Text implements Styleable, Parseable
 
     public static function parse(string $string): self
     {
-        return new self(array_map(function (string $line) {
-            return Line::parse($line);
-        }, explode("\n", $string)));
+        $placeholder = sprintf('[new_line_%s]', uniqid());
+
+        $string = preg_replace_callback('/<[^>]+>.*?<\/>/s', function ($matches) use ($placeholder) {
+            return str_replace("\n", $placeholder, $matches[0]);
+        }, $string);
+
+        // We can only break into new lines at the breaks that are outside of tags.
+        $lines = array_map(function (string $line) use ($placeholder) {
+            return Line::parse(str_replace($placeholder, "\n", $line));
+        }, explode("\n", $string ?? ''));
+
+        return new self($lines);
     }
 
     public static function fromLine(Line $line): self
