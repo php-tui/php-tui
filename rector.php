f02c700440435885ef2366b533caff18ce6805a6
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-11-19 21:18:39 -0300

    Add rector

---
+++ rector.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodingStyle\Rector\ArrowFunction\StaticArrowFunctionRector;
+use Rector\Config\RectorConfig;
+use Rector\Php53\Rector\Ternary\TernaryToElvisRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->parallel();
+    $rectorConfig->importShortClasses();
+
+    $rectorConfig->paths([
+        __DIR__ . '/lib',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->skip([
+        TernaryToElvisRector::class,
+        ClosureToArrowFunctionRector::class,
+    ]);
+
+    $rectorConfig->rules([
+        StaticArrowFunctionRector::class,
+    ]);
+
+    $rectorConfig->sets([
+        SetList::TYPE_DECLARATION,
+        LevelSetList::UP_TO_PHP_81,
+    ]);
+};
