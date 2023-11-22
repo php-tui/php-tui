cbceb2e23d702c04e7ce519ae52bed3f965d53be
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-11-21 21:40:15 -0300

    Add new methods to `Padding`

--- src/Extension/Core/Widget/Block/Padding.php
+++ src/Extension/Core/Widget/Block/Padding.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace PhpTui\Tui\Extension\Core\Widget\Block;
 
-class Padding
+final class Padding
 {
-    private function __construct(public int $left, public int $right, public int $top, public int $bottom)
-    {
+    private function __construct(
+        public int $left,
+        public int $right,
+        public int $top,
+        public int $bottom
+    ) {
     }
 
     public static function none(): self
@@ -25,4 +29,41 @@ class Padding
         return new self($left, $right, $top, $bottom);
     }
 
+    public static function vertical(int $amount): self
+    {
+        return new self(0, 0, $amount, $amount);
+    }
+
+    public static function horizontal(int $amount): self
+    {
+        return new self($amount, $amount, 0, 0);
+    }
+
+    public function left(int $left): self
+    {
+        $this->left = $left;
+
+        return $this;
+    }
+
+    public function right(int $right): self
+    {
+        $this->right = $right;
+
+        return $this;
+    }
+
+    public function top(int $top): self
+    {
+        $this->top = $top;
+
+        return $this;
+    }
+
+    public function bottom(int $bottom): self
+    {
+        $this->bottom = $bottom;
+
+        return $this;
+    }
 }
