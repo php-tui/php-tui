a0ec387e3050e201d32d6246aa43b3614f2af37f
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-11-22 08:38:51 -0300

    Add new builder methods to `Margin` (#167)
    * Add new builder methods to `Margin`
    
    * Update CHANGELOG.md

--- src/Model/Margin.php
+++ src/Model/Margin.php
@@ -4,10 +4,36 @@ declare(strict_types=1);
 
 namespace PhpTui\Tui\Model;
 
-class Margin
+final class Margin
 {
-    public function __construct(public int $vertical, public int $horizontal)
-    {
+    public function __construct(
+        public readonly int $vertical,
+        public readonly int $horizontal
+    ) {
     }
 
+    public static function none(): self
+    {
+        return new self(0, 0);
+    }
+
+    public static function all(int $amount): self
+    {
+        return new self($amount, $amount);
+    }
+
+    public static function fromScalars(int $vertical, int $horizontal): self
+    {
+        return new self($vertical, $horizontal);
+    }
+
+    public static function vertical(int $vertical): self
+    {
+        return new self($vertical, 0);
+    }
+
+    public static function horizontal(int $horizontal): self
+    {
+        return new self(0, $horizontal);
+    }
 }
