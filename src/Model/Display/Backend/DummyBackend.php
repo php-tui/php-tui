f20d7654f657117989954141ae3ff1a095dc576e
Daniel Leech <daniel@dantleech.com>
2023-11-28 17:29:47 +0000

    Update

--- src/Model/Display/Backend/DummyBackend.php
+++ src/Model/Display/Backend/DummyBackend.php
@@ -19,6 +19,10 @@ final class DummyBackend implements Backend
 
     private ?string $flushed = null;
 
+    /** 
+     * @param int<0,max> $height
+     * @param int<0,max> $width
+     */
     public function __construct(private int $width, private int $height, private Position $cursorPosition = new Position(0, 0))
     {
         $this->fillGrid($width, $height);
@@ -58,12 +62,20 @@ final class DummyBackend implements Backend
         }, $this->grid);
     }
 
+    /**
+     * @param int<0,max> $width
+     * @param int<0,max> $height
+     */
     public static function fromDimensions(int $width, int $height): self
     {
 
         return new self($width, $height);
     }
 
+    /**
+     * @param int<0,max> $width
+     * @param int<0,max> $height
+     */
     public function setDimensions(int $width, int $height): void
     {
         $this->fillGrid($width, $height);
