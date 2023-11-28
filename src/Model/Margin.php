3ff46b3c5d32e22a038fbc134a6fea64d45071ea
Daniel Leech <daniel@dantleech.com>
2023-11-28 17:23:21 +0000

    Revert "area"
    This reverts commit 57442b77f2218886219d028e083038bdbb88be09.

--- src/Model/Margin.php
+++ src/Model/Margin.php
@@ -7,13 +7,7 @@ namespace PhpTui\Tui\Model;
 final class Margin
 {
     public function __construct(
-        /**
-         * @var int<0,max>
-         */
         public readonly int $vertical,
-        /**
-         * @var int<0,max>
-         */
         public readonly int $horizontal
     ) {
     }
@@ -23,33 +17,21 @@ final class Margin
         return new self(0, 0);
     }
 
-    /**
-     * @param int<0,max> $amount
-     */
     public static function all(int $amount): self
     {
         return new self($amount, $amount);
     }
 
-    /**
-     * @param int<0,max> $vertical
-     */
     public static function fromScalars(int $vertical, int $horizontal): self
     {
         return new self($vertical, $horizontal);
     }
 
-    /**
-     * @param int<0,max> $vertical
-     */
     public static function vertical(int $vertical): self
     {
         return new self($vertical, 0);
     }
 
-    /**
-     * @param int<0,max> $horizontal
-     */
     public static function horizontal(int $horizontal): self
     {
         return new self(0, $horizontal);
