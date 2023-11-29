e6af0a6de5f590760abafa5eb6d977b37648a313
Daniel Leech <daniel@dantleech.com>
2023-11-29 16:45:18 +0000

    Tests are passing

--- src/Extension/Core/Widget/SparklineRenderer.php
+++ src/Extension/Core/Widget/SparklineRenderer.php
@@ -2,7 +2,11 @@
 
 namespace PhpTui\Tui\Extension\Core\Widget;
 
+use PhpTui\Tui\Extension\Core\Widget\Sparkline\RenderDirection;
 use PhpTui\Tui\Model\Display\Buffer;
+use PhpTui\Tui\Model\Math\VectorUtil;
+use PhpTui\Tui\Model\Position\Position;
+use PhpTui\Tui\Model\Symbol\BarSet;
 use PhpTui\Tui\Model\Widget;
 use PhpTui\Tui\Model\WidgetRenderer;
 
@@ -12,6 +16,48 @@ class SparklineRenderer implements WidgetRenderer
     {
         if (!$widget instanceof SparklineWidget) {
             return;
+        }
+
+        $area = $buffer->area();
+        if ($area->height < 1) {
+            return;
+        }
+        $max = $widget->max ?? VectorUtil::max($widget->data) ?? 0;
+        $maxIndex = min($area->width, count($widget->data));
+        $data = array_map(function (int $e) use ($max, $area) {
+            if ($max === 0) {
+                return 0;
+            }
+            return intval(round($e * $area->height * 8 / $max));
+        }, array_slice($widget->data, 0, $maxIndex));
+
+        for ($j = $area->height - 1; $j >= 0; $j--) {
+            $i = 0;
+            foreach ($data as $value) {
+                $symbol = match ($value) {
+                    0 => BarSet::EMPTY,
+                    1 => BarSet::ONE_EIGHTH,
+                    2 => BarSet::ONE_QUARTER,
+                    3 => BarSet::THREE_EIGHTHS,
+                    4 => BarSet::HALF,
+                    5 => BarSet::FIVE_EIGHTHS,
+                    6 => BarSet::THREE_QUARTERS,
+                    7 => BarSet::SEVEN_EIGHTHS,
+                    default => BarSet::FULL,
+                };
+                $x = match ($widget->direction) {
+                    RenderDirection::LeftToRight => $area->left() + $i,
+                    RenderDirection::RightToLeft => max(0, $area->right() - $i - 1),
+                };
+                $buffer->get(Position::at(
+                    $x,
+                    $area->top() + $j
+                ))
+                    ->setChar($symbol)
+                    ->setStyle($widget->style);
+
+                $i++;
+            }
         }
     }
 }
--- tests/Unit/Extension/Core/Widget/SparklineRendererTest.php
+++ tests/Unit/Extension/Core/Widget/SparklineRendererTest.php
@@ -6,17 +6,19 @@ namespace PhpTui\Tui\Tests\Unit\Extension\Core\Widget;
 
 use Generator;
 use PhpTui\Tui\Extension\Core\Widget\SparklineWidget;
+use PhpTui\Tui\Extension\Core\Widget\Sparkline\RenderDirection;
 use PhpTui\Tui\Model\Area;
+use PhpTui\Tui\Model\Direction;
 use PhpTui\Tui\Model\Display\Buffer;
 use PhpTui\Tui\Model\Widget;
 
 final class SparklineRendererTest extends WidgetTestCase
 {
     /**
-     * @dataProvider provideTableRender
+     * @dataProvider provideSparklineRender
      * @param array<int,string> $expected
      */
-    public function testTableRender(Area $area, Widget $widget, array $expected): void
+    public function testSparklineRender(Area $area, Widget $widget, array $expected): void
     {
         $buffer = Buffer::empty($area);
         $this->render($buffer, $widget);
@@ -25,7 +27,7 @@ final class SparklineRendererTest extends WidgetTestCase
     /**
      * @return Generator<array{Area,Widget,list<string>}>
      */
-    public static function provideTableRender(): Generator
+    public static function provideSparklineRender(): Generator
     {
         yield 'default' => [
             Area::fromDimensions(20, 2),
@@ -37,13 +39,19 @@ final class SparklineRendererTest extends WidgetTestCase
            ,
         ];
         yield 'sparkline' => [
-            Area::fromDimensions(20, 2),
-            SparklineWidget::fromData(10, 1, 2, 3),
+            Area::fromDimensions(12, 1),
+            SparklineWidget::fromData(...range(0, 8)),
             [
-                '▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅▅',
-                '                    ',
+                ' ▁▂▃▄▅▆▇█   ',
             ]
            ,
+        ];
+        yield 'right to left' => [
+            Area::fromDimensions(12, 1),
+            SparklineWidget::fromData(...range(0, 8))->direction(RenderDirection::RightToLeft),
+            [
+                "   █▇▆▅▄▃▂▁ ",
+            ]
         ];
     }
 }
